62254617e31e2924b3b25d9d6d65cf270679a0dd
Claas Ahlrichs <claasahl@gmail.com>
2019-05-02 14:13:06 +0200

    enabled incremental updates on apartments

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,13 @@
 import TelegramBot from "node-telegram-bot-api";
 import puppeteer from "puppeteer";
 import {CronJob} from "cron";
-import express, {Request, Response} from "express";
+import express from "express";
 import bodyParser from "body-parser";
 
 // replace the value below with the Telegram token you receive from @BotFather
 const TOKEN = process.env.TOKEN || "";
 const PORT = process.env.PORT;
-const PROJECT_DOMAIN = process.env.PROJECT_DOMAIN;
+const HOST = process.env.HOST;
 const SUCCESS_STICKER_ID = process.env.SUCCESS_STICKER_ID || "";
 const FAILED_STICKER_ID = process.env.FAILED_STICKER_ID || "";
 const PARENTS = (process.env.PARENTS || "").split(",");
@@ -16,15 +16,12 @@ const EXECUTABLE = process.env.PUPPETEER_EXECUTABLE
 const apartments = new Map<string, Apartment>();
 
 // Create a bot that uses 'polling' to fetch new updates
-const bot = new TelegramBot(TOKEN);
-if(PROJECT_DOMAIN) {
-    bot.setWebHook(`https://${PROJECT_DOMAIN}.glitch.me/bot${TOKEN}`);
-}
-
+const bot = new TelegramBot(TOKEN, {polling: true});
 
 // Listen for "/apartments" messages.
 bot.onText(/\/apartments/, async (msg) => {
     const chatId = msg.chat.id;
+    console.log(JSON.stringify(msg, null, 2))
     if (isFromParent(msg)) {
         try {
             await fetchAndPublishApartments(chatId)
@@ -47,16 +44,6 @@ function isFromParent(msg: TelegramBot.Message): boolean {
     return false;
 }
 
-async function fetchAndPublishApartments(chatId: number) {
-    const fetched = await fetchApartments();
-    for (const apartment of fetched) {
-        if(apartments && !apartments.has(apartment.link)) {
-            apartments.set(apartment.link, apartment)
-            await bot.sendPhoto(chatId, apartment.screenshot)
-            await bot.sendMessage(chatId, apartment.link)
-        }
-    }
-}
 
 interface Apartment {
     name: string,
@@ -64,40 +51,62 @@ interface Apartment {
     screenshot: Buffer | string
 }
 
-async function fetchApartments(): Promise<Apartment[]> {
-    const apartments: Apartment[] = []
-    const options = typeof EXECUTABLE === "undefined" ? {args: ["--no-sandbox"]} : {executablePath: EXECUTABLE,args: ["--no-sandbox"]}
-    const browser = await puppeteer.launch(options);
+interface ApartmentLink {
+    name: string,
+    link: string
+}
 
-    // look for links to apartments
+async function launchBrowser(): Promise<puppeteer.Browser> {
+    const options = typeof EXECUTABLE === "undefined" ? {args: ["--no-sandbox"]} : {executablePath: EXECUTABLE,args: ["--no-sandbox"]}
+    return puppeteer.launch(options);
+}
+
+async function fetchApartmentLinks(browser: puppeteer.Browser): Promise<ApartmentLink[]> {
+    const apartmentLinks:ApartmentLink[] = []
     const mainPage = await browser.newPage();
     await mainPage.goto('https://wahlinfastigheter.se/lediga-objekt/parkering/', { waitUntil: "networkidle2" });
     const links = await mainPage.$x("//h3/a[contains(@href, '/lediga-objekt/')]");
-
-    // open each apartment in a new page
     for(const link of links) {
-        const property = await link.getProperty("href")
-        const url = await property.jsonValue()
-
-        const apartmentPage = await browser.newPage()
-        await apartmentPage.goto(url,{waitUntil: "networkidle2"})
-        await apartmentPage.click(".new-cookies-button")
-        await apartmentPage.waitFor(500);
-        const headers = await apartmentPage.$x("//div[@class='fastighet']/div/h2")
-        let name = "";
-        if(headers.length > 0) {
-            const property = await headers[0].getProperty("innerText")
-            name = await property.jsonValue()
-        }
-
-        const descriptions = await apartmentPage.$x('//div[@class="fastighet"]')
-        const photo = descriptions.length > 0 ? await descriptions[0].screenshot() : "";
-        apartments.push({ name, link: url, screenshot: photo })
-        
-        await apartmentPage.close()
+        const titleProp = await link.getProperty("title")
+        const name = await titleProp.jsonValue();
+        const hrefProp = await link.getProperty("href")
+        const url = await hrefProp.jsonValue();
+        apartmentLinks.push({name, link: url})
     }
-    await browser.close();
-    return apartments
+    await mainPage.close();
+    return Promise.resolve(apartmentLinks);
+}
+
+async function fetchApartment(browser: puppeteer.Browser, apartment: ApartmentLink): Promise<Apartment> {
+    const apartmentPage = await browser.newPage()
+    await apartmentPage.goto(apartment.link,{waitUntil: "networkidle2"})
+    await apartmentPage.click(".new-cookies-button")
+    await apartmentPage.waitFor(500);
+    const headers = await apartmentPage.$x("//div[@class='fastighet']/div/h2")
+    let name = apartment.name;
+    if(headers.length > 0) {
+        const property = await headers[0].getProperty("innerText")
+        name = await property.jsonValue()
+    }
+
+    const descriptions = await apartmentPage.$x('//div[@class="fastighet"]')
+    const photo = descriptions.length > 0 ? await descriptions[0].screenshot() : "";
+    
+    await apartmentPage.close()
+    return Promise.resolve({ name, link: apartment.link, screenshot: photo })
+}
+
+async function fetchAndPublishApartments(chatId: number): Promise<void> {
+    const browser = await launchBrowser();
+    const links = await fetchApartmentLinks(browser);
+    for(const link of links) {
+        try {
+        const apartment = await fetchApartment(browser, link)
+        await bot.sendPhoto(chatId, apartment.screenshot, {caption: apartment.link})
+        } catch(error) {
+            await bot.sendMessage(chatId, link.link).catch(() => {});
+        }
+    }
 }
 
 // automatically fetch and publish apartments
@@ -115,21 +124,16 @@ job.start();
 
 
 
+if (HOST) {
+    bot.setWebHook(`${HOST}/bot${TOKEN}`);
 
-
-
-const app = express()
-
-// parse the updates to JSON
-app.use(bodyParser.json());
-
-// We are receiving updates at the route below!
-app.post(`/bot${TOKEN}`, (req: Request, res: Response) => {
-  bot.processUpdate(req.body);
-  res.sendStatus(200);
-});
-
-// Start Express Server
-app.listen(PORT, () => {
-  console.log(`Express server is listening on ${PORT}`);
-});
+    const app = express()
+    app.use(bodyParser.json());
+    app.post(`/bot${TOKEN}`, (req, res) => {
+        bot.processUpdate(req.body);
+        res.sendStatus(200);
+    });
+    app.listen(PORT, () => {
+        console.log(`Server is listening on ${PORT}`);
+    });
+}
