fa68f403683b720e89860ceb501fe0138ddb1fa6
Claas Ahlrichs <claasahl@gmail.com>
2019-05-02 14:34:07 +0200

    extracted "Wahlin" functions into dedicated file

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,9 @@
 import TelegramBot from "node-telegram-bot-api";
-import puppeteer from "puppeteer";
 import { CronJob } from "cron";
 import express from "express";
 import bodyParser from "body-parser";
+
+import * as wahlin from "./wahlin";
 
 // replace the value below with the Telegram token you receive from @BotFather
 const TOKEN = process.env.TOKEN || "";
@@ -13,7 +14,7 @@ const FAILED_STICKER_ID = process.env.FAILED_STICKER_ID || "";
 const PARENTS = (process.env.PARENTS || "").split(",");
 const CHAT_ID = (process.env.CHAT_ID as number | undefined) || -1;
 const EXECUTABLE = process.env.PUPPETEER_EXECUTABLE;
-const apartments = new Map<string, Apartment>();
+const apartments = new Map<string, wahlin.Apartment>();
 
 // Create a bot that uses 'polling' to fetch new updates
 const polling = typeof HOST === "undefined" || typeof PORT === "undefined";
@@ -46,74 +47,16 @@ function isFromParent(msg: TelegramBot.Message): boolean {
   return false;
 }
 
-interface Apartment {
-  name: string;
-  link: string;
-  screenshot: Buffer | string;
-}
 
-interface ApartmentLink {
-  name: string;
-  link: string;
-}
 
-async function launchBrowser(): Promise<puppeteer.Browser> {
-  const options =
-    typeof EXECUTABLE === "undefined"
-      ? { args: ["--no-sandbox"] }
-      : { executablePath: EXECUTABLE, args: ["--no-sandbox"] };
-  return puppeteer.launch(options);
-}
 
-async function fetchApartmentLinks(
-  browser: puppeteer.Browser
-): Promise<ApartmentLink[]> {
-  const apartmentLinks: ApartmentLink[] = [];
-  const mainPage = await browser.newPage();
-  await mainPage.goto("https://wahlinfastigheter.se/lediga-objekt/parkering/", {
-    waitUntil: "networkidle2"
-  });
-  const links = await mainPage.$x("//h3/a[contains(@href, '/lediga-objekt/')]");
-  for (const link of links) {
-    const titleProp = await link.getProperty("title");
-    const name = await titleProp.jsonValue();
-    const hrefProp = await link.getProperty("href");
-    const url = await hrefProp.jsonValue();
-    apartmentLinks.push({ name, link: url });
-  }
-  await mainPage.close();
-  return Promise.resolve(apartmentLinks);
-}
-
-async function fetchApartment(
-  browser: puppeteer.Browser,
-  apartment: ApartmentLink
-): Promise<Apartment> {
-  const apartmentPage = await browser.newPage();
-  await apartmentPage.goto(apartment.link, { waitUntil: "networkidle2" });
-  await apartmentPage.click(".new-cookies-button");
-  await apartmentPage.waitFor(500);
-  const headers = await apartmentPage.$x("//div[@class='fastighet']/div/h2");
-  let name = apartment.name;
-  if (headers.length > 0) {
-    const property = await headers[0].getProperty("innerText");
-    name = await property.jsonValue();
-  }
-
-  const descriptions = await apartmentPage.$x('//div[@class="fastighet"]');
-  const photo =
-    descriptions.length > 0 ? await descriptions[0].screenshot() : "";
-
-  await apartmentPage.close();
-  return Promise.resolve({ name, link: apartment.link, screenshot: photo });
-}
 
 async function fetchAndPublishApartments(chatId: number): Promise<void> {
-  const browser = await launchBrowser();
-  const links = await fetchApartmentLinks(browser);
+  const browser = await wahlin.launchBrowser();
+  const links = await wahlin.fetchApartmentLinks(browser);
   for (const link of links) {
     try {
-      const apartment = await fetchApartment(browser, link);
+      const apartment = await wahlin.fetchApartment(browser, link);
       await bot.sendPhoto(chatId, apartment.screenshot, {
         caption: apartment.link
       });
